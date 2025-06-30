e11db6472c818fbecfb4b2214797201f209b75a7
orxfun <orx.ugur.arikan@gmail.com>
2025-06-30 11:15:18 +0200

    init composed criteria

---
+++ src/composed_criteria.rs
@@ -0,0 +1,25 @@
+use crate::criterion::Criterion;
+
+pub struct ComposedCriteria<X1, X2>(X1, X2)
+where
+    X1: Criterion,
+    X2: Criterion<Problem = X1::Problem>;
+
+impl<X1, X2> ComposedCriteria<X1, X2>
+where
+    X1: Criterion,
+    X2: Criterion<Problem = X1::Problem>,
+{
+}
+
+// impl<X1, X2> Criterion for ComposedCriteria<X1, X2>
+// where
+//     X1: Criterion,
+//     X2: Criterion<Problem = X1::Problem>,
+// {
+//     type Problem = X1::Problem;
+
+//     type Input<'a> = (X1::Input<'a>, X2::Input<'a>);
+
+//     type MoveGenerator;
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod candidate_move;
+mod composed_criteria;
 mod criterion;
 mod r#move;
 mod move_generator;
