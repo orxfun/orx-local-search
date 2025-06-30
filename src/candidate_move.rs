b0d2e9378e3323f93747095f4afd12c389a166d4
orxfun <orx.ugur.arikan@gmail.com>
2025-06-30 11:12:15 +0200

    candidate move constructor

--- src/candidate_move.rs
+++ src/candidate_move.rs
@@ -15,15 +15,19 @@ impl<P> CandidateMove<P>
 where
     P: Problem,
 {
+    pub fn new(r#move: MoveOf<P>, objective_value: ObjectiveUnitOf<P>) -> Self {
+        Self {
+            r#move,
+            objective_value,
+        }
+    }
+
     pub fn compose(self, other: CandidateMove<P>) -> Self {
         debug_assert_eq!(&self.r#move, &other.r#move);
         let objective_value = <P::ObjectiveValue as ObjectiveValue>::reduce(
             self.objective_value,
             other.objective_value,
         );
-        Self {
-            r#move: self.r#move,
-            objective_value,
-        }
+        Self::new(self.r#move, objective_value)
     }
 }
