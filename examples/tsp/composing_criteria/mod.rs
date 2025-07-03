562c409dc249ddfe043a9b31740b986358adb28d
orxfun <orx.ugur.arikan@gmail.com>
2025-07-03 06:07:24 +0200

    examples renamed

--- examples/tsp/composing_criteria/mod.rs
+++ examples/tsp/composing_criteria/mod.rs
@@ -1,5 +1,5 @@
-pub mod composition3;
-pub mod composition5;
-pub mod explicit1;
-pub mod explicit3;
-pub mod explicit5;
+pub mod composition_3criteria;
+pub mod composition_5criteria;
+pub mod explicit_3criteria;
+pub mod explicit_5criteria;
+pub mod single_criterion;
--- examples/tsp/main.rs
+++ examples/tsp/main.rs
@@ -16,12 +16,15 @@ pub use tour::Tour;
 pub use tour_after_move::TourAfterInsertIter;
 pub use tour_cost::TourCost;
 
-use crate::composing_criteria::{composition3, composition5, explicit1, explicit3, explicit5};
+use crate::composing_criteria::{
+    composition_3criteria, composition_5criteria, explicit_3criteria, explicit_5criteria,
+    single_criterion,
+};
 
 fn main() {
-    explicit1::run();
-    explicit3::run();
-    explicit5::run();
-    composition3::run();
-    composition5::run();
+    single_criterion::run();
+    explicit_3criteria::run();
+    explicit_5criteria::run();
+    composition_3criteria::run();
+    composition_5criteria::run();
 }
