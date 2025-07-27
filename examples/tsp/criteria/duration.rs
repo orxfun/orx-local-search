6b2dbf171d82018de857197ad9b610581df721fa
orxfun <orx.ugur.arikan@gmail.com>
2025-07-27 18:56:47 +0200

    duration criterion example

---
+++ examples/tsp/criteria/duration.rs
@@ -0,0 +1,59 @@
+use crate::{tour::Tour, tour_cost::TourCost, tsp::Tsp};
+use orx_iterable::Collection;
+use orx_local_search::{Criterion, Objective};
+
+#[derive(Default, Clone, Copy)]
+pub struct Duration;
+
+impl Criterion for Duration {
+    type Input = DurationMatrix;
+
+    type Solution = Tour;
+
+    type Objective = TourCost;
+
+    type Problem = Tsp;
+
+    fn evaluate(
+        self,
+        input: &Self::Input,
+        tour: &Self::Solution,
+    ) -> Option<<Self::Objective as Objective>::Unit> {
+        Some(input.tour_cost(tour))
+    }
+}
+
+pub struct DurationMatrix(Vec<Vec<u64>>);
+
+impl DurationMatrix {
+    pub fn new(matrix: Vec<Vec<u64>>) -> Self {
+        Self(matrix)
+    }
+
+    pub fn get(&self, from_city: usize, to_city: usize) -> u64 {
+        self.0[from_city][to_city]
+    }
+
+    pub fn tour_len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn tour_cost(&self, tour: &Tour) -> u64 {
+        let mut cost = 0;
+        for p in 0..tour.iter().len().saturating_sub(1) {
+            cost += self.get(tour[p], tour[p + 1]);
+        }
+        cost
+    }
+
+    pub fn example_input() -> Self {
+        Self(vec![
+            vec![0, 30, 60, 20, 5, 120],
+            vec![20, 0, 10, 50, 80, 70],
+            vec![10, 50, 0, 30, 40, 10],
+            vec![50, 70, 60, 0, 140, 70],
+            vec![10, 80, 30, 50, 0, 20],
+            vec![100, 20, 50, 50, 80, 0],
+        ])
+    }
+}
